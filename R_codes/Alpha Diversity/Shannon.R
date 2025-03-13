ba303fb5f3f02742951433f814fc2adc67e2231a
QingRuK <99295681+QingRuK@users.noreply.github.com>
2025-03-12 21:55:00 -0700

    Update and rename R_codes/Shannon and Observed Feature alpha diversity.R to R_codes/Alpha Diversity/Shannon.R

--- R_codes/Alpha Diversity/Shannon.R
+++ R_codes/Alpha Diversity/Shannon.R
@@ -68,37 +68,4 @@ ggplot(shannon_df_pre, aes(x = location, y = Shannon, fill = location)) +
 ##Shannon: Pre-Treatment Kruskall-Wallis ##
 kruskal.test(Shannon ~ location, data = shannon_df_pre)
 
-#### Observed Features ####
-observed_diversity <- estimate_richness(pj2_mod, measures = c("Observed"))
-observed_df <- data.frame(sample_data(pj2_mod), observed_diversity)
-observed_df
-ggplot(observed_df, aes(x = group, y = Observed, fill = group)) + 
-  geom_boxplot() + 
-  theme_minimal() + 
-  labs(title = "Observed Features by Group") +
-  facet_wrap(~location, scales = 'free')
-
-##Observed: Kruskall-Wallis##
-kruskal.test(Observed ~ group, data = observed_df)
-
-##Observed: ANOVA##
-observed_phylo_location <- unique(observed_df$location)
-
-for (loc in observed_phylo_location) {
-  cat("\nANOVA for Location:", loc, "\n")
-  subset_data <- subset(observed_df, location == loc)
-  lm_pd_vs_group_log <- lm(log(Observed) ~ group, data=subset_data)
-  anova_pd_vs_group_log <- aov(lm_pd_vs_group_log)
-  summary(anova_pd_vs_group_log)
-  print(TukeyHSD(anova_pd_vs_group_log))
-}
-
-#Observed: Significant groups
-  #Spleen
-    #Pre-ICI vs Post-ICI1
-    #Pre-ICI vs Post-ICI2
-    #Pre-ICI vs Post-ICI3
-  #Tumor
-    #Pre-ICI vs Post-ICI3
-    #Post-ICI1 vs Post-ICI3
 
