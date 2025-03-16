f06e7ff6225648795a6468fc53fc9cb91b885eaf
P-singh25 <pranjalis125@gmail.com>
2025-03-15 19:17:54 -0700

    edited picrust2

---
+++ R_codes/PiCRUST2/picrust_analysis.R
@@ -0,0 +1,121 @@
+##### Install Necessary Packages Once #####
+
+# Install BiocManager
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+# Create a list of all the packages needed to install and then install them 
+pkgs <- c("ALDEx2", "SummarizedExperiment", "Biobase", "devtools", 
+          "ComplexHeatmap", "BiocGenerics", "metagenomeSeq", 
+          "Maaslin2", "edgeR", "lefser", "limma", "KEGGREST", "DESeq2")
+
+for (pkg in pkgs) {
+   if (!requireNamespace(pkg, quietly = TRUE))
+       BiocManager::install(pkg)
+}
+# when asked if you want to update all, some or none please type "n" for none
+
+# After installing all of its above dependencies, Install ggpicrust2
+install.packages("ggpicrust2")
+
+
+
+##### Load packages #####
+
+# Load all necessary libraries
+library(readr)
+library(ggpicrust2)
+library(tibble)
+library(tidyverse)
+library(ggprism)
+library(patchwork)
+library(DESeq2)
+library(ggh4x)
+
+
+
+##### Import files and preparing tables #####
+
+
+#Importing the pathway PICrsut2
+abundance_file <- "PiCRUST2/pathway_abundance.tsv"
+abundance_data <- read_delim(abundance_file, delim = "\t", col_names = TRUE, trim_ws = TRUE, skip = 1)
+abundance_data  =as.data.frame(abundance_data)
+
+#Import the metadata file
+metadata <- read_delim("PiCRUST2/melanoma_metadata_updated_copy.tsv")
+
+# Filter the metadata for only 'Pre-ICI' and 'Post-ICI3' groups
+filtered_metadata <- metadata %>%
+  filter(group %in% c("Pre-ICI", "Post-ICI3"))
+
+# Save the filtered metadata to as a new file
+write_delim(filtered_metadata, "PiCRUST2/filtered_metadata_for_picrust.tsv", delim = "\t")
+
+#Removing samples with no data(0) in the abundance data
+abundance_data_1 =  abundance_data[, colSums(abundance_data != 0) > 0]
+
+#Ensuring the rownames for the abundance_data_filtered is empty. This is required for their functions to run.
+rownames(abundance_data_1) = NULL
+
+#verify samples in metadata match samples in abundance_data
+abun_samples = rownames(t(abundance_data_1[,-1])) #Getting a list of the sample names in the newly filtered abundance data
+metadata = filtered_metadata[filtered_metadata$`sample-id` %in% abun_samples,] #making sure the filtered metadata only includes these samples
+
+
+#### DESEq ####
+#Perform pathway DAA using DESEQ2 method
+abundance_daa_results_df <- pathway_daa(abundance = abundance_data_1 %>% rownames_to_column("#OTU ID"), 
+                                        metadata = metadata, group = "group", daa_method = "DESeq2")
+
+# Annotate MetaCyc pathway so they are more descriptive
+metacyc_daa_annotated_results_df <- pathway_annotation(pathway = "MetaCyc", 
+                                                       daa_results_df = abundance_daa_results_df, ko_to_kegg = FALSE)
+
+# Filter p-values to only significant ones
+feature_with_p_0.05 <- abundance_daa_results_df %>% filter(p_values < 0.05)
+
+#Changing the pathway column to description for the results 
+feature_desc = inner_join(feature_with_p_0.05,metacyc_daa_annotated_results_df, by = "feature")
+feature_desc$feature = feature_desc$description
+feature_desc = feature_desc[,c(1:7)]
+colnames(feature_desc) = colnames(feature_with_p_0.05)
+
+#Changing the pathway column to description for the abundance table
+abundance = abundance_data_filtered %>% filter(pathway %in% feature_with_p_0.05$feature)
+colnames(abundance)[1] = "feature"
+abundance_desc = inner_join(abundance,metacyc_daa_annotated_results_df, by = "feature")
+abundance_desc$feature = abundance_desc$description
+#this line will change for each dataset. 34 represents the number of samples in the filtered abundance table
+abundance_desc = abundance_desc[,-c(34:ncol(abundance_desc))] 
+
+# Generate a heatmap
+pathway_heatmap(abundance = abundance_desc %>% column_to_rownames("feature"), metadata = metadata, group = "subject")
+
+# Generate pathway PCA plot
+pathway_pca(abundance = abundance_data_filtered %>% column_to_rownames("pathway"), metadata = metadata, group = "subject")
+
+# Generating a bar plot representing log2FC from the custom deseq2 function
+
+# Go to the Deseq2 function script and update the metadata category of interest
+
+# Lead the function in
+source("DESeq2_function.R")
+
+# Run the function on your own data
+res =  DEseq2_function(abundance_data_filtered, metadata, "subject")
+res$feature =rownames(res)
+res_desc = inner_join(res,metacyc_daa_annotated_results_df, by = "feature")
+res_desc = res_desc[, -c(8:13)]
+View(res_desc)
+
+# Filter to only include significant pathways
+sig_res = res_desc %>%
+  filter(pvalue < 0.05)
+# You can also filter by Log2fold change
+
+sig_res <- sig_res[order(sig_res$log2FoldChange),]
+ggplot(data = sig_res, aes(y = reorder(description, sort(as.numeric(log2FoldChange))), x= log2FoldChange, fill = pvalue))+
+  geom_bar(stat = "identity")+ 
+  theme_bw()+
+  labs(x = "Log2FoldChange", y="Pathways")
