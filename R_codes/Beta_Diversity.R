7c76400b176d957df891d374705709c52e534ac6
Ran's Macbook Air <ransmacbookair@Rans-Persocom.local>
2025-03-30 11:23:27 -0700

    update bray curtis

--- R_codes/Beta_Diversity.R
+++ R_codes/Beta_Diversity.R
@@ -194,11 +194,30 @@ uu_dm_group <- distance(pj2_group, method = "bray")
 perm_results_post3 <- adonis2(uu_dm_group ~ location, data = meta_data_group, permutations = 999)
 print(perm_results_post3)
 
+###significance of spleen vs others
+# Create a new column in metadata: "Spleen" vs. "Others"
+sample_data(pj2_filtered)$location_binary <- ifelse(sample_data(pj2_filtered)$location == "Spleen", "Spleen", "Other")
 
+# Ensure it is a factor
+sample_data(pj2_filtered)$location_binary <- factor(sample_data(pj2_filtered)$location_binary, levels = c("Spleen", "Other"))
 
+# Subset Pre-ICI group
+pj2_group_1 <- subset_samples(pj2_filtered, group == "Pre-ICI")
+meta_data_group <- as(sample_data(pj2_group_1), "data.frame")
+uu_dm_group <- distance(pj2_group_1, method = "bray")
 
+# PERMANOVA comparing "Spleen" vs. "Other" locations
+perm_results_pre <- adonis2(uu_dm_group ~ location_binary, data = meta_data_group, permutations = 999)
+print(perm_results_pre)
 
+# Subset Post-ICI3 group
+pj2_group <- subset_samples(pj2_filtered, group == "Post-ICI3")
+meta_data_group <- as(sample_data(pj2_group), "data.frame")
+uu_dm_group <- distance(pj2_group, method = "bray")
 
+# PERMANOVA comparing "Spleen" vs. "Other" locations
+perm_results_post3 <- adonis2(uu_dm_group ~ location_binary, data = meta_data_group, permutations = 999)
+print(perm_results_post3)
 
 
 #unweighted
